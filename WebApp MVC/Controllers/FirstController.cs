eb143d84aee47c7c34a022e1961909a9b446d9bd
skatic7 <katicstanko@gmail.com>
2018-07-28 15:41:18 +0200

    Adding controller

---
+++ WebApp MVC/Controllers/FirstController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_MVC.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello" + name + ", numTimes is: " + numTimes);//used for malicious software
+        }
+    }
+}
